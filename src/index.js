89bc8c47b73acd41f135253e2b3fe500b6b778f2
AbreuNatali <abreu.natali@gmail.com>
2022-08-13 00:22:01 +0300

    feat: towelSort function

--- src/index.js
+++ src/index.js
@@ -1,5 +1,24 @@
+function towelSort(matrix) {
+  let result = [];
+    for (let i=0; i<matrix.length; i++) {
+    let matrixItem = matrix[i];
+    if (i%2 === 0) {
+      result = result.concat(matrix[i]);        
+      }       
+    else {
+        result = result.concat(matrix[i].reverse());        
+      }  
+}
+return result;
+}
 
-// You should implement your task here.
+/*
+console.log(towelSort([
+  [1, 2, 4],
+  [5, 6, 7, 8],
+  [9, 12],
+]));
+*/
 
 module.exports = function towelSort (matrix) {
   return [];
